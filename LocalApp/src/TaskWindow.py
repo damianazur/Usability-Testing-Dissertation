022cd06b99ae3d7e59e6eacbacae3add40997acf
Damian Wojtowicz <47143782+damianazur@users.noreply.github.com>
2021-01-30 20:00:05 +0000

    Code refactored, move on to next task complete, need to refactor more code in preperation for question rendering

---
+++ LocalApp/src/TaskWindow.py
@@ -0,0 +1,187 @@
+import requests
+import json
+from win32api import GetSystemMetrics
+import win32gui as win32gui
+
+from TaskWindow import TaskWindow
+
+from PyQt5.QtCore import Qt
+import PyQt5.QtCore as QtCore
+import PyQt5.QtGui as QPalette
+from PyQt5.QtGui import QFont, QStandardItemModel, QStandardItem
+from PyQt5.QtWidgets import (
+    QMainWindow, 
+    QPushButton, 
+    QApplication, 
+    QLabel, 
+    QLineEdit, 
+    QVBoxLayout, 
+    QHBoxLayout, 
+    QWidget, 
+    QDialogButtonBox, 
+    QGroupBox, 
+    QFormLayout, 
+    QComboBox, 
+    QSpinBox, 
+    QListWidget, 
+    QScrollArea)
+
+
+class TaskWindow(QWidget):
+    def __init__(self, taskData, previousTaskPos):
+        QWidget.__init__(self, None, Qt.WindowStaysOnTopHint)
+        self.previousTaskPos = previousTaskPos
+
+        self.taskData = taskData
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(self.mainLayout)
+
+        self.renderTaskWindow()
+        self.renderTask(taskData)
+
+
+    def renderTaskWindow(self):
+        # Setting dimensions
+        self.screenW = GetSystemMetrics(0)
+        self.screenH = GetSystemMetrics(1)
+        self.windowW = 300
+        self.windowH = 170
+        self.windowPaddingLeft = 30
+        self.windowsPaddingTop = 70
+
+        if self.previousTaskPos == None:
+            self.initX = self.screenW - self.windowW - self.windowPaddingLeft
+            self.initY = self.windowsPaddingTop
+        else:
+            self.initX = self.previousTaskPos.x()
+            self.initY = self.previousTaskPos.y()
+
+        self.buttonMinW = 100
+        self.buttonHeight = 25
+
+        # Configurations
+        self.setWindowFlag(Qt.FramelessWindowHint) 
+        self.setGeometry(self.initX, self.initY, self.windowW, self.windowH)
+        self.setStyleSheet("font-size: 14px;")
+        self.dragPos = QtCore.QPoint(self.pos().x(), self.pos().y())
+        self.hideButtonDragged = False
+
+        # Hide button
+        hideButton = QPushButton("Hide", self)
+        hideButton.clicked.connect(self.toggleHide)
+        hideButton.resize(hideButton.sizeHint())
+        hideButton.setStyleSheet("background-color: rgb(32, 123, 207 );")
+        hideButton.pressed.connect(self.hideButtonPress)
+        hideButton.mouseMoveEvent = self.hideButtonDrag
+        self.hideButton = hideButton
+        self.mainLayout.addWidget(self.hideButton)
+
+
+    def renderTask(self, taskData):
+        jsonSteps = json.loads(taskData["stepsJSON"])
+
+        # Create instruction text body
+        stepIndex = 1
+        stepString = "Task: " + taskData["taskName"] + "\n\n"
+        for step in jsonSteps:
+            stepString += str(stepIndex) + ".\n" + (str(step["value"]) + " ") * 20 + "\n\n"
+            stepIndex += 1
+
+
+        # Set the text
+        taskLayout = ScrollLabel(self) 
+        taskLayout.setText(stepString) 
+        taskLayout.setGeometry(0, 40, 300, 200) 
+        taskLayout.setContentsMargins(0, 0, 0, 0)
+
+        # Add "Finish Task" button
+        hbox = QHBoxLayout()
+        nextTaskButton = QPushButton("Finish Task", self)
+        nextTaskButton.clicked.connect(self.nextTask)
+        nextTaskButton.resize(nextTaskButton.sizeHint())
+        nextTaskButton.setStyleSheet("background-color: rgb(32, 207, 76);")
+        nextTaskButton.setFixedWidth(100)
+        hbox.setAlignment(Qt.AlignCenter)
+        hbox.addWidget(nextTaskButton)
+        taskLayout.lay.addLayout(hbox)
+        self.taskLayout = taskLayout
+
+        self.mainLayout.addWidget(self.taskLayout)
+    
+
+    def hideButtonPress(self):
+        _, _, (x,y) = win32gui.GetCursorInfo()
+        self.dragPos = QtCore.QPoint(x, y)
+
+    def hideButtonDrag(self, event):
+        if event.buttons() == QtCore.Qt.LeftButton:
+            self.hideButtonDragged = True
+            self.move(self.pos() + event.globalPos() - self.dragPos)
+            self.dragPos = event.globalPos()
+            self.previousTaskPos = self.pos()
+
+
+    # Hiding the content of the task with the button
+    def toggleHide(self, e):
+        currentState = self.hideButton.text()
+
+        # If the hideButton was dragged (the window was moved when the button was selected)
+        # then do not activate the button. Button will only be activated on click (no drag before click)
+        if (self.hideButtonDragged):
+            self.hideButtonDragged = False
+            return
+
+        if (currentState == "Hide"):
+            self.hideButton.setText('Show')
+            self.setFixedSize(self.buttonMinW, self.buttonHeight)
+            x = int(self.pos().x() + self.windowW - self.buttonMinW)
+            y = int(self.pos().y())
+            self.setGeometry(x, y, 0, 0)
+            self.textBody.hide()
+
+        elif (currentState == "Show"):
+            self.hideButton.setText('Hide')
+            self.setFixedSize(self.windowW, self.windowH)
+            x = int(self.pos().x() - self.windowW + self.buttonMinW)
+            y = int(self.pos().y())
+            self.setGeometry(x, y, 0, 0)
+            self.textBody.show()
+            
+
+    # Clicking on the body of the window before it is dragged and repositioned
+    # The click position needs to be saved to know where to move the window to
+    def mousePressEvent(self, event):
+        self.dragPos = event.globalPos()
+    
+    # When the user drags the window
+    def mouseMoveEvent(self, event):
+        if event.buttons() == QtCore.Qt.LeftButton:
+            self.move(self.pos() + event.globalPos() - self.dragPos)
+            self.dragPos = event.globalPos()
+            self.previousTaskPos = self.pos()
+    
+    
+    # User clicked finish task
+    def nextTask(self, e):
+        print("Next Task")
+
+
+# Scroll Label is used for the body of the task
+class ScrollLabel(QScrollArea): 
+    def __init__(self, *args, **kwargs): 
+        QScrollArea.__init__(self, *args, **kwargs) 
+        self.setWidgetResizable(True) 
+  
+        content = QWidget(self) 
+        self.setWidget(content) 
+  
+        self.lay = QVBoxLayout(content) 
+        self.label = QLabel(content) 
+
+        self.label.setAlignment(Qt.AlignLeft | Qt.AlignTop) 
+        self.label.setWordWrap(True) 
+        self.lay.addWidget(self.label) 
+  
+    def setText(self, text): 
+        self.label.setText(text) 
--- LocalApp/src/main.py
+++ LocalApp/src/main.py
@@ -1,21 +1,46 @@
 import requests
 import json
 
+from functools import partial
+
+from TestBodyWindow import TestBodyWindow
+
 from PyQt5.QtCore import Qt
 import PyQt5.QtCore as QtCore
 import PyQt5.QtGui as QPalette
 from PyQt5.QtGui import QFont, QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QWidget, QDialogButtonBox, QGroupBox, QFormLayout, QComboBox, QSpinBox, QListWidget, QScrollArea
+from PyQt5.QtWidgets import (
+    QMainWindow, 
+    QPushButton, 
+    QApplication, 
+    QLabel, 
+    QLineEdit, 
+    QVBoxLayout, 
+    QHBoxLayout, 
+    QWidget, 
+    QDialogButtonBox, 
+    QGroupBox, 
+    QFormLayout, 
+    QComboBox, 
+    QSpinBox, 
+    QListWidget, 
+    QScrollArea)
 
 
 class Window(QWidget):
+    LOCAL_APP_API = "http://localhost:8090/api/localapp/"
+    GET_TEST_BY_REF_CODE_ENTRY = "getTestDetailsByReferenceCode/"
+
+
     def __init__(self):
         QWidget.__init__(self, None, Qt.WindowStaysOnTopHint)
-        # self.setWindowFlag(Qt.FramelessWindowHint) 
+
+        # Window configurations
         self.setGeometry(400, 350, 500, 250)
         self.setWindowTitle("UsabCheck")
         self.setStyleSheet("font-size: 14px;")
         
+        # Layout
         self.mainLayout = QVBoxLayout()
         self.mainLayout.setContentsMargins(20, 20, 200, 0)
         self.createTestEnterLayout()
@@ -24,6 +49,7 @@ class Window(QWidget):
         self.setLayout(self.mainLayout)
 
 
+    # Creates the first part of the form where the user enters a test reference code
     def createTestEnterLayout(self):
         layout = QFormLayout()
         
@@ -32,7 +58,7 @@ class Window(QWidget):
         btn = QPushButton("Submit", self)
         btn.clicked.connect(self.submitRefCode)
         btn.resize(btn.sizeHint())
-        btn.setStyleSheet("background-color: rgb(32, 123, 207 );")
+        btn.setStyleSheet("background-color: rgb(32, 123, 207);")
         btn.setFixedWidth(100)
 
         layout.addRow(refCodeLabel)
@@ -41,38 +67,37 @@ class Window(QWidget):
         layout.setContentsMargins(0, 0, 0, 30)
 
         self.mainLayout.addLayout(layout)
-        # self.mainLayout.addWidget(btn)
     
 
+    # When the user submits the code get the data
     def submitRefCode(self, e):
         refCode = self.refCodeInput.text()
         if refCode == "":
             refCode = "H8VH1NLA"
 
-        print("Submitted", refCode)
-
         sendData = {"referenceCode": str(refCode)}
-        request = requests.post("http://localhost:8090/api/localapp/getTestDetailsByReferenceCode", data=sendData)
+        request = requests.post(self.LOCAL_APP_API + self.GET_TEST_BY_REF_CODE_ENTRY, data=sendData)
         data = json.loads(request.text)
         print(data)
 
         self.displayTestData(data)
 
     
+    # Display the data so that the user can verify that the test is correct
     def displayTestData(self, data):
         testName = data["testName"]
         createdDate = data["launchedDate"]
         noOfTasks = 0
         noOfQuestions = 0
 
+        # Counte the number of tasks and questions
         for item in data["sequenceData"]:
             if ("questionConfigsJSON" in item.keys()):
                 noOfQuestions += 1
             if ("stepsJSON" in item.keys()):
                 noOfTasks += 1
-        
-        print(testName, createdDate, noOfTasks, noOfQuestions)
 
+        # Remove the widget if it already exists (if the user submits another code the data is reloaded)
         self.mainLayout.removeWidget(self.displayTestLayout)
         self.displayTestLayout = QGroupBox("Usability Test Details")
         layout = QFormLayout()
@@ -84,13 +109,13 @@ class Window(QWidget):
         self.displayTestLayout.setLayout(layout)
         
         self.mainLayout.addWidget(self.displayTestLayout)
-
         
+        # Form that asks the user for their info
         layout = QFormLayout()
         refCodeLabel = QLabel("Your name:")
         self.refCodeInput = QLineEdit()
         btn = QPushButton("Begin", self)
-        btn.clicked.connect(self.begin, data)
+        btn.clicked.connect(partial(self.begin, data))
         btn.resize(btn.sizeHint())
         btn.setStyleSheet("background-color: rgb(32, 207, 76);")
         btn.setFixedWidth(100)
@@ -103,114 +128,11 @@ class Window(QWidget):
         self.mainLayout.addLayout(layout)
 
 
-    def begin(self, e, data):
-        self.window = OtherWindow(data)
+    def begin(self, data):
+        self.window = TestBodyWindow(data)
         self.window.show()
         self.close()
 
-
-class OtherWindow(QWidget):
-    def __init__(self, data):
-        QWidget.__init__(self, None, Qt.WindowStaysOnTopHint)
-        self.setWindowFlag(Qt.FramelessWindowHint) 
-        self.setGeometry(1600, 70, 300, 200)
-        self.setWindowTitle("Instructions")
-        self.setStyleSheet("font-size: 14px;")
-        self.dragPos = QtCore.QPoint()
-
-        self.sequenceIndex = 4
-        self.renderTask(data)
-    
-
-    def renderTask(self, data):
-        self.mainLayout = QVBoxLayout()
-        self.mainLayout.setContentsMargins(0, 0, 0, 0)
-        
-        hideButton = QPushButton("Hide", self)
-        hideButton.clicked.connect(self.hide)
-        hideButton.resize(hideButton.sizeHint())
-        hideButton.setStyleSheet("background-color: rgb(32, 123, 207 );")
-        self.mainLayout.addWidget(hideButton)
-        
-        sequenceData = data["sequenceData"][self.sequenceIndex]
-        jsonSteps = json.loads(sequenceData["stepsJSON"])
-        
-        print(jsonSteps)
-
-        stepIndex = 1
-        stepString = ""
-        for step in jsonSteps:
-            stepString += str(stepIndex) + ".\n" + (str(step["value"]) + " ") * 20 + "\n\n"
-            stepIndex += 1
-
-        label = ScrollLabel(self) 
-        label.setText(stepString) 
-        label.setGeometry(0, 40, 300, 200) 
-        label.setContentsMargins(0, 0, 0, 0)
-
-        hbox = QHBoxLayout()
-        nextTaskButton = QPushButton("Finish Task", self)
-        nextTaskButton.clicked.connect(self.hide)
-        nextTaskButton.resize(nextTaskButton.sizeHint())
-        nextTaskButton.setStyleSheet("background-color: rgb(32, 207, 76);")
-        nextTaskButton.setFixedWidth(100)
-        hbox.setAlignment(Qt.AlignCenter)
-        hbox.addWidget(nextTaskButton)
-        label.lay.addLayout(hbox)
-
-        self.mainLayout.addWidget(label)
-
-        # self.mainLayout.addWidget(label)
-        self.setLayout(self.mainLayout)
-    
-    def hide(self, e):
-        print("Hide")
-
-    def mousePressEvent(self, event):
-        self.dragPos = event.globalPos()
-        
-
-    def mouseMoveEvent(self, event):
-        if event.buttons() == QtCore.Qt.LeftButton:
-            self.move(self.pos() + event.globalPos() - self.dragPos)
-            self.dragPos = event.globalPos()
-            event.accept()
-        
-
-
-# class for scrollable label 
-class ScrollLabel(QScrollArea): 
-  
-    # contructor 
-    def __init__(self, *args, **kwargs): 
-        QScrollArea.__init__(self, *args, **kwargs) 
-  
-        # making widget resizable 
-        self.setWidgetResizable(True) 
-  
-        # making qwidget object 
-        content = QWidget(self) 
-        self.setWidget(content) 
-  
-        # vertical box layout 
-        self.lay = QVBoxLayout(content) 
-  
-        # creating label 
-        self.label = QLabel(content) 
-  
-        # setting alignment to the text 
-        self.label.setAlignment(Qt.AlignLeft | Qt.AlignTop) 
-  
-        # making label multi-line 
-        self.label.setWordWrap(True) 
-  
-        # adding label to the layout 
-        self.lay.addWidget(self.label) 
-  
-    # the setText method 
-    def setText(self, text): 
-        # setting text to the label 
-        self.label.setText(text) 
 
 def showGUI():
     app = QApplication([])
@@ -222,8 +144,8 @@ def showGUI():
     sendData = {"referenceCode": str(refCode)}
     request = requests.post("http://localhost:8090/api/localapp/getTestDetailsByReferenceCode", data=sendData)
     data = json.loads(request.text)
-
-    window = OtherWindow(data)
+    data["sequenceData"].sort(key=lambda obj: obj["sequenceNumber"], reverse=False)
+    window = TestBodyWindow(data)
     window.show()
 
     app.exec_()
