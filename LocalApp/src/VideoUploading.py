5203ed01b548ed04b65b942892c5b913ebe2cd40
Damian Wojtowicz <47143782+damianazur@users.noreply.github.com>
2021-02-11 00:33:25 +0000

    Added bar chart but it breaks the tab

--- LocalApp/src/VideoUploading.py
+++ LocalApp/src/VideoUploading.py
@@ -6,15 +6,22 @@ if __name__ == '__main__':
     print("----------- Starting Video Upload ----------- ")
 
     client = vimeo.VimeoClient(
-      token='{8617caf723060007eeee7b26e1ded3b9}',
-      key='{68c0d97e7b6fa360e3e4e87079ab02eef6198078}',
-      secret='{DtXMmxJ47Wz4TExYAn+09Tr+EF0ItDioZ3kU1nOv8L5VmZDfZ0BAIcV1nUFxDJuF7goYFMSjCFdrMANy4Xzaid45/0tvdUdgbhzwJjH9yLjuFQPDMRIonKXf/3q13XcT}'
+      token='cd878f7f019c963e813fc639661661e7',
+      key='68c0d97e7b6fa360e3e4e87079ab02eef6198078',
+      secret='DtXMmxJ47Wz4TExYAn+09Tr+EF0ItDioZ3kU1nOv8L5VmZDfZ0BAIcV1nUFxDJuF7goYFMSjCFdrMANy4Xzaid45/0tvdUdgbhzwJjH9yLjuFQPDMRIonKXf/3q13XcT'
     )
 
-    uri = "https://api.vimeo.com/oauth/authorize"
+    # uri = "https://api.vimeo.com/oauth/authorize"
     # uri = "https://api.vimeo.com/oauth/access_token"
-
-    response = client.get(uri)
-    print(response)
+    # response = client.get(uri)
+    # print(response)
     # print(json.loads(response))
+
+    file_name = './videos/uploadVideo.avi'
+    uri = client.upload(file_name, data={
+      'name': 'First Video',
+      'description': 'No Description'
+    })
+
+    print('Your video URI is: %s' % (uri))
         
--- WebApp/usabcheck/frontend/src/tabs/testResultOverview.component.js
+++ WebApp/usabcheck/frontend/src/tabs/testResultOverview.component.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import DropdownGenerator from "components/dropdownGenerator.component";
 import Server from "services/server.service";
+import ApexCharts from 'apexcharts'
 
 export class TestResultOverviewTab extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isShown: false,
+      isShown: true,
       selectedTask: {
         taskName: "Choose Task",
         taskId: undefined
@@ -41,12 +42,13 @@ export class TestResultOverviewTab extends Component {
 
     console.log("Selected task: ", params)
 
-    this.updateTaskList(params.taskId);
     this.setState({
       selectedTask: {
         taskName: taskName,
         taskId: params.taskId}
       });
+
+      
   }
 
   generateTaskDropdown = (onSelectFunction, selectedTaskState) => {
@@ -72,13 +74,91 @@ export class TestResultOverviewTab extends Component {
     )
   }
 
+  renderBarChart() {
+    // var series = [{
+    //   data: [21, 22, 10, 28, 16, 21, 13, 30]
+    // }];
+
+    // var options= {
+    //   chart: {
+    //     height: 350,
+    //     type: 'bar',
+    //     events: {
+    //       click: function(chart, w, e) {
+    //         // console.log(chart, w, e)
+    //       }
+    //     }
+    //   },
+    //   colors: colors,
+    //   plotOptions: {
+    //     bar: {
+    //       borderRadius: 6,
+    //       columnWidth: '45%',
+    //       distributed: true,
+    //     }
+    //   },
+    //   dataLabels: {
+    //     enabled: false
+    //   },
+    //   legend: {
+    //     show: false
+    //   },
+    //   xaxis: {
+    //     categories: [
+    //       ['John', 'Doe'],
+    //       ['Joe', 'Smith'],
+    //       ['Jake', 'Williams'],
+    //       'Amber',
+    //       ['Peter', 'Brown'],
+    //       ['Mary', 'Evans'],
+    //       ['David', 'Wilson'],
+    //       ['Lily', 'Roberts'], 
+    //     ],
+    //     labels: {
+    //       style: {
+    //         colors: colors,
+    //         fontSize: '12px'
+    //       }
+    //     }
+    //   }
+    // }
+
+    // return (
+    //   <div id="chart">
+    //     <ApexCharts options={this.state.options} series={this.state.series} type="bar" height={350} />
+    //   </div>
+    // );
+  }
+
+  renderChart() {
+    var options = {
+      chart: {
+        type: 'line'
+      },
+      series: [{
+        name: 'sales',
+        data: [30,40,35,50,49,60,70,91,125]
+      }],
+      xaxis: {
+        categories: [1991,1992,1993,1994,1995,1996,1997,1998,1999]
+      }
+    }
+    
+    var chart = new ApexCharts(document.querySelector("#chartX2"), options);
+    
+    chart.render();
+  }
+
   render() {
     return (
       <React.Fragment>
         {this.state.isShown ? (
-          <div>
+          <div style={{display: "none"}}>
             Overview content goes here
             {this.generateTaskDropdown(this.onTaskSelect.bind(this), this.state.selectedTask.taskName)}
+            <div id="chartX2">
+            </div>
+            {this.renderChart()}
           </div>
         ) : ( 
           null
